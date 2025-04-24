0b6143db067ec7a8be7a00e48aeb051e5bba1c92
aman <aman_k@ormae.com>
2025-04-24 23:32:14 +0530

    Fix utils module structure and imports

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,10 @@
 """
 Utils package for the VRP application.
-""" 
+"""
+from .utils import (
+    create_distance_matrix,
+    get_download_link,
+    create_folium_map,
+    create_plotly_map,
+    haversine_distance
+) 
